ea81e7e0e09e1ee9676ffa9745b3f7c9779efb9b
cartasuzuki <carlo@cartasuzuki.com>
2022-04-01 09:36:59 +0900

    iot

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
 import pandas as pd
-from pypfopt import EfficientFrontier
-from pypfopt import risk_models
-from pypfopt import expected_returns
+import PortfolioOptimizer
 import YJDownloadNikkei50Prices
 
 
 
-def OptimizePortfolio(prices):
-    mu = expected_returns.mean_historical_return(prices)
-    S = risk_models.sample_cov(prices)
-    # Optimize for maximal Sharpe ratio
-    ef = EfficientFrontier(mu, S)
-    raw_weights = ef.max_sharpe()
-    cleaned_weights = ef.clean_weights()
-    ef.save_weights_to_file("weights.csv")  # saves to file
-    print(cleaned_weights)
-    ef.portfolio_performance(verbose=True)
 
-def formatNikkeiPricesCSC(prices):
-    return
 
 
 # Read in price data
@@ -27,7 +13,7 @@ stockprices = YJDownloadNikkei50Prices.FixFormat(stockprices)
 
 print(stockprices.head())
 
-OptimizePortfolio(stockprices)
+weights, sharpe, ret = PortfolioOptimizer.PortfolioOptimizer.optimize_portfolio(stockprices,0,0.25)
 
 
 
